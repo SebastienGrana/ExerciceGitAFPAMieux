66342dae772f03f59f7d083144e1b6fc81771c36
granarolo sébastien <granarolo.sebastien@gmail.com>
2018-11-12 09:42:01 +0100

    creation branch D1, OverTheTopClassD1, and test in Main

--- src/model/Main.java
+++ src/model/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		OverTheTopClassD1 d1 = new OverTheTopClassD1();
+		
+		d1.bestBrint(25, "Toto");
 	}
 
 }
